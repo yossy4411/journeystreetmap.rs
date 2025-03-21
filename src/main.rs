821d54298bac1cd5f48a749e290ac43ff238a0a8
yossy4411 <yossy4411.dev@gmail.com>
2025-03-21 18:00:16 +0900

    チャンクを読み込み、コンソールに書き出す処理を追加

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,26 @@ mod journeymap;
 
 fn main() {
     let mut reader = journeymap::JourneyMapReader::new("/home/okayu/.local/share/ModrinthApp/profiles/Fabulously Optimized/journeymap/data/mp/160~251~235~246/");
-    let chunk_x = 0;
-    let chunk_z = 0;
-    let chunk = reader.get_chunk_at(chunk_x, chunk_z).expect("Failed to read region");
+    reader.read_region(-1, -1).expect("Failed to read region");
 
-    if chunk.is_none() {
-        println!("Chunk not found");
-        return;
-    } let chunk = chunk.unwrap();
-
-    println!("Chunk pos: {}", chunk.pos);
-
-
-    for i in 0..=chunk_x {
-        for j in 0..=chunk_z {
-            let chunk = reader.get_chunk_at(i, j).expect("Failed to read region");
-            if chunk.is_none() {
-                println!("Chunk not found");
-                return;
-            } let chunk = chunk.unwrap();
-            println!("Chunk pos: {}", chunk.pos);
-            let mut vec = chunk.get_all_sorted();
-            for ((x, y), data) in vec {
-                println!("{}, {}: {:?}", x, y, data.biome_name);
+    let stopwatch = std::time::Instant::now();
+    for i in 0..=31 {
+        for j in 0..=31 {
+            if let Ok(chunk) = reader.get_chunk(i, j) {
+                if chunk.is_none() {
+                    println!("Chunk not found");
+                    continue;
+                } let chunk = chunk.unwrap();
+                // println!("Chunk pos: {}", chunk.pos);
+                let mut vec = chunk.get_all_sorted();
+                for ((x, y), data) in vec {
+                    // println!("{}, {}: {:?}", x, y, data.biome_name);
+                }
+            } else {
+                println!("Chunk load failed");
+                continue;
             }
         }
     }
+    println!("Time taken: {:?}", stopwatch.elapsed());
 }
