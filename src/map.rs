d4bf7dd790378d2471be6b1239d013f0f6ae00e1
yossy4411 <yossy4411.dev@gmail.com>
2025-04-14 18:55:09 +0900

    画像の読み込みを非同期かつマルチスレッドで行うように変更

--- src/map.rs
+++ src/map.rs
@@ -2,9 +2,11 @@ use fastanvil::asyncio::Region;
 use journeystreetmap::journeymap::{biome, JourneyMapReader};
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::sync::Arc;
 use tokio::fs::File;
 use macroquad::math::{Vec2};
 use macroquad::prelude::Texture2D;
+use tokio::sync::Mutex;
 use journeystreetmap::journeymap::biome::RGB;
 
 #[derive(Debug, Clone)]
@@ -37,105 +39,97 @@ pub enum EditingType {
 
 #[derive(Debug, Default)]
 pub struct JourneyMapViewerState {
-    images: HashMap<(i32, i32), Texture2D>,  // Regionごとの画像データをキャッシュするためのHashMap
     edit_mode: EditingMode,
     editing_type: EditingType,
     editable: bool,
     path: Vec<(f32, f32)>,
 }
 
-impl JourneyMapViewerState {
-    pub async fn load_images(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut reader = JourneyMapReader::new("/home/okayu/.local/share/ModrinthApp/profiles/Fabulously Optimized/journeymap/data/mp/160~251~235~246/");
-        let region_offset_x = 0;
-        let region_offset_z = 0;
+pub async fn load_images(images: Arc<Mutex<HashMap<(i32, i32), Texture2D>>>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut reader = JourneyMapReader::new("/home/okayu/.local/share/ModrinthApp/profiles/Fabulously Optimized/journeymap/data/mp/160~251~235~246/");
+    let region_offset_x = 0;
+    let region_offset_z = 0;
 
-        let stopwatch = std::time::Instant::now();
+    let stopwatch = std::time::Instant::now();
 
-        let mut threads = Vec::new();
-        let regions = // reader.get_regions_list();
-            [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)];
+    let mut threads = Vec::new();
+    let regions = // reader.get_regions_list();
+        [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)];
 
-        for (i, (region_x, region_z)) in regions.into_iter().enumerate() {
-            let region = reader.try_read_region(region_offset_x + region_x, region_offset_z + region_z).await;
-            if region.is_some() {
-                let thr = tokio::spawn(async move {
-                    Self::buffer_region(region.unwrap(), region_offset_x, region_offset_z, region_x, region_z).await
-                });
-                threads.push(thr);
-            } else {
-                println!("Region not found");
-                continue;
-            }
-            if i > 20 {
-                break;
-            }
+    for (i, (region_x, region_z)) in regions.into_iter().enumerate() {
+        let region = reader.try_read_region(region_offset_x + region_x, region_offset_z + region_z).await;
+        if region.is_some() {
+            let thr = tokio::spawn(async move {
+                buffer_region(region.unwrap(), region_offset_x, region_offset_z, region_x, region_z).await
+            });
+            threads.push(thr);
+        } else {
+            println!("Region not found");
+            continue;
         }
-
-        for thr in threads {
-            let (key, content) = thr.await.unwrap();
-            let texture = Texture2D::from_rgba8(512, 512, &content);
-            self.images.insert(key, texture);
+        if i > 20 {
+            break;
         }
-        println!("Time taken: {:?}", stopwatch.elapsed());
-        Ok(())
     }
 
-    async fn buffer_region(mut region: Region<File>, region_offset_x: i32, region_offset_z: i32, region_x: i32, region_z: i32) -> ((i32, i32), Vec<u8>) {
-        let mut image_data = [RGB::default(); 512 * 512];
-        for i in 0..=31 {
-            for j in 0..=31 {
-                let chunk_result = JourneyMapReader::get_chunk(&mut region, i, j).await;
-                match chunk_result {
-                    Err(..) => {
+    for thr in threads {
+        let (key, content) = thr.await.unwrap();
+        let texture = Texture2D::from_rgba8(512, 512, &content);
+        images.lock().await.insert(key, texture);  // 画像を保存
+    }
+    println!("Time taken: {:?}", stopwatch.elapsed());
+    Ok(())
+}
+
+async fn buffer_region(mut region: Region<File>, region_offset_x: i32, region_offset_z: i32, region_x: i32, region_z: i32) -> ((i32, i32), Vec<u8>) {
+    let mut image_data = [RGB::default(); 512 * 512];
+    for i in 0..=31 {
+        for j in 0..=31 {
+            let chunk_result = JourneyMapReader::get_chunk(&mut region, i, j).await;
+            match chunk_result {
+                Err(..) => {
+                    continue;
+                }
+                Ok(chunk) => {
+                    if chunk.is_none() {
+                        println!("Chunk not found");
                         continue;
                     }
-                    Ok(chunk) => {
-                        if chunk.is_none() {
-                            println!("Chunk not found");
-                            continue;
-                        }
-                        let chunk = chunk.unwrap();
-                        for (pos, data) in chunk.sections {
-                            let mut splited = pos.split(',');
-                            let x: i32 = splited.next().unwrap().parse().unwrap();
-                            let z: i32 = splited.next().unwrap().parse().unwrap();
+                    let chunk = chunk.unwrap();
+                    for (pos, data) in chunk.sections {
+                        let mut splited = pos.split(',');
+                        let x: i32 = splited.next().unwrap().parse().unwrap();
+                        let z: i32 = splited.next().unwrap().parse().unwrap();
 
-                            // ブロック座標をリージョン内の相対座標に変換
-                            let pixel_x = x - 512 * (region_offset_x + region_x);
-                            let pixel_y = z - 512 * (region_offset_z + region_z);
+                        // ブロック座標をリージョン内の相対座標に変換
+                        let pixel_x = x - 512 * (region_offset_x + region_x);
+                        let pixel_y = z - 512 * (region_offset_z + region_z);
 
-                            // RGBA配列のインデックスを計算
-                            let i = (pixel_y * 512 + pixel_x) as usize;
+                        // RGBA配列のインデックスを計算
+                        let i = (pixel_y * 512 + pixel_x) as usize;
 
-                            // iが画像内に入るなら色を設定
-                            if i < 512 * 512 {
-                                let color = biome::get_color(&data.biome_name);
-                                image_data[i] = color;
-                            }
+                        // iが画像内に入るなら色を設定
+                        if i < 512 * 512 {
+                            let color = biome::get_color(&data.biome_name);
+                            image_data[i] = color;
                         }
                     }
                 }
             }
         }
-        let mut colors = Vec::with_capacity(512 * 512 * 4);  // RGBA8
-        for i in 0..512 * 512 {
-            let color = image_data[i];
-            colors.push(color.r);
-            colors.push(color.g);
-            colors.push(color.b);
-            colors.push(255); // Alpha
-        }
-        ((region_x, region_z), colors)
     }
+    let mut colors = Vec::with_capacity(512 * 512 * 4);  // RGBA8
+    for i in 0..512 * 512 {
+        let color = image_data[i];
+        colors.push(color.r);
+        colors.push(color.g);
+        colors.push(color.b);
+        colors.push(255); // Alpha
+    }
+    ((region_x, region_z), colors)
 }
 
 impl JourneyMapViewerState {
-    /// 画像の参照を返す
-    pub fn images(&self) -> &HashMap<(i32, i32), Texture2D> {
-        &self.images
-    }
-
     pub fn editing_type(&self) -> EditingType {
         self.editing_type
     }
