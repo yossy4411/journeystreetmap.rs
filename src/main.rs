0971e145a1a9e8c21d7c80681c3efc75e39deaa5
yossy4411 <yossy4411.dev@gmail.com>
2025-04-28 19:17:30 +0900

    停止

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use fastanvil::asyncio::Region;
-use pmtiles2::{Compression, TileType};
+use pmtiles2::{Compression, PMTiles, TileType};
 use tokio::fs::File;
 use tokio::task::JoinSet;
 use journeystreetmap::journeymap::biome::RGB;
@@ -63,7 +63,7 @@ async fn main() {
     let mut status = Status::new("Convert to WEBP".to_string(), images.len() as u32);
 
     let mut webp_images = Vec::new();
-    for (pos, image) in images.iter() {
+    for (pos, image) in images.into_iter() {
         let encoder = webp::Encoder::from_rgba(image.as_slice(), 512, 512);
         let webp_image = encoder.encode_lossless();
         webp_images.push((pos, webp_image));
@@ -73,8 +73,36 @@ async fn main() {
     info!("Finished converting WEBP images.");
 
     info!("[3/4] Pack WEBP images into PMTiles");
-    let pt = pmtiles2::PMTiles::new(TileType::WebP, Compression::GZip);
+    let mut pt = pmtiles2::PMTiles::new(TileType::WebP, Compression::GZip);
     // todo! implement this
+    for ((region_x, region_z), webp) in webp_images.into_iter() {
+        let id = pmtiles2::util::tile_id(region_x, region_z);
+        pt.add_tile(id, webp.to_vec());
+    }
+
+    // 結論から言うと、かなり現実的ではない。 -- MinecraftのデータをGSIにするということは、まず先駆者が居ないため、参考となる資料がない。
+    // このリポジトリは私の2ヶ月をかけた些細な挑戦である。
+    // 歴史は繰り返すというが、私と同じ考えに至って同じような挑戦をする人が現れるが、そのときにはこのリポジトリを参考にしてみてほしい。
+    // ここに「実現不可だと判断した理由」を載せる。
+    
+    // 1. PMTilesの仕様上
+    // PMTilesは、タイルのIDを指定して、タイルを追加する必要がある。タイルのIDは、x座標とz座標を元に計算されるため、x座標とz座標を指定してタイルを追加する必要がある。
+    // このx座標とz座標は、符号なし64bit整数で指定すべきであり、Minecraftのリージョンは符号あり32bit整数のタイル座標のようなものであるため、PMTilesの仕様上、リージョンを追加することはできない。
+    // 対処法は、リージョン座標に一定の基準を設け、すべての座標を0以上の値のタプルに変換すること。
+    
+    // 2. エディターの作成
+    // UXを良くするためには、エディターを設けるのが最善であるが、これを１から作成するのは非常に難しいと判断できる。なぜなら、これを作成するためには、icedなどのGUIライブラリを使用する必要があるが、RustのGUIライブラリはどれもゲームには向いていないためである。
+    // 様々なGUIライブラリを試したが、どれも相性が悪かった。特にIME（日本語入力）が実装されているものに絞ると、選択肢がかなり少なくなる。
+    
+    // 3. タスク
+    // これは完全に私の怠慢であるが、このプロジェクトを進めるための時間がなくなった。別の進行中のプロジェクトに食い込む形で進めていたが、予想よりも多くの時間がかかっており、これを進めるための時間がもうない。
+    // これを作る間にもアイデアがたくさん浮かんだので、それを実現していこうと思う。
+    // もしこのプロジェクトを代わりに進めてくれる人が居たら、ぜひ連絡してほしい。
+    
+    // 4. おわりに
+    // 誰か私の構想を実現してくれることを願っている。興味があればぜひ連絡をしてみてほしい。
+    // https://x.com/yossy4411_dev
+    
 }
 
 async fn buffer_region(mut region: Region<File>, region_offset_x: i32, region_offset_z: i32, region_x: i32, region_z: i32) -> ((i32, i32), Box<[u8;512 * 512 * 4]>) {
